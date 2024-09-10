23fd79d09fa906ff2eb0aab196ba99056ef0cb32
s-n-1-0 <72431055+s-n-1-0@users.noreply.github.com>
2024-09-10 22:29:37 +0900

    remove comment

--- binary_bci2000_processing/Preprocessing.py
+++ binary_bci2000_processing/Preprocessing.py
@@ -4,7 +4,6 @@ from scipy.signal import butter, filtfilt
 
 
 def preprocess(data,fs):
-    #print(f"{scaler.mean_} / {scaler.scale_}")
     # バンドパスフィルタを適用
     if True:
         # バンドパスフィルタの設定
@@ -19,17 +18,4 @@ def preprocess(data,fs):
     # データを標準化
     scaler = StandardScaler()
     data = scaler.fit_transform(data.T).T
-    # パワースペクトルへの変換
-    #freq, data = periodogram(data, fs=fs)
-    #"""
-    #freq_range = [5, 20]  # 使用する周波数範囲
-    # 特定の周波数範囲以内のインデックスを取得
-    #in_range_idx = np.logical_and(freq >= freq_range[0], freq <= freq_range[1])
-    # 特定の周波数範囲以内の特徴を残す
-    #data = data[:, in_range_idx]
-    #"""
-    # 圧縮
-    #new_data = []
-    #for i in range(0, data.shape[0],20):
-    #    new_data.append(np.mean(data[i:i+chunk_size], axis=0))
     return np.array(data)
